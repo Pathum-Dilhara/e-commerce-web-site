e5a73d4d9cd5d7605d0c95ce424d800922a294a6
Pathum Dilhara <hbgpathumdilhara@gmail.com>
2025-03-02 20:42:51 +0530

    upload files

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './DescriptionBox.css'
+
+    const DescriptionBox = ()=> {
+        return (
+            <div className='discriptionbox'>
+               <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+                </div> 
+                <div className="descriptionbox-description">
+                    <p>aaaaaaaaaaaa</p>
+                    <p>bbbbbbbbbbbb</p>
+                </div>
+            </div>
+        );
+    }
+
+export default DescriptionBox;
---
+++ src/Pages/Cart.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CartItems from "../Components/Cartitems/Cartitems.jsx";
+
+const Cart = ()=>{
+    return(
+    <div>
+        <CartItems/>
+    </div>
+    )
+}
+
+export default Cart
